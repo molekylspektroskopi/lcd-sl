58799315b2cf2faf2a7aad8d5c2305a79d53966a
Markus Wiik <vassius@gmail.com>
2013-05-05 00:49:16 +0200

    Add regex to read part of inconsistent string

--- sl.py
+++ sl.py
@@ -41,10 +41,20 @@ col = TextColours.ORANGE
 try:
     api = SLAPI('http://www1.sl.se/realtidws/RealTimeService.asmx?wsdl')
     last_check = 0
+    r = re.compile(r'([0-9]{2} [^ ]+ [0-9]{2}(:[0-9]{2}|.{4}))')
     while(True):
         if time.time() - last_check >= 15:
             c = api.get_departures(9301)
-            s2 = "".join(c[3]).split(',')[0]
+            m = re.match(r, "".join(c[1]))
+            if m:
+                s1 = m.group(0)
+            else:
+                s1 = ""
+            m = re.match(r, "".join(c[3]))
+            if m:
+                s2 = m.group(0)
+            else:
+                s2 = ""
             last_check = time.time()
             try:
                 f.close()
@@ -52,7 +62,7 @@ try:
                 pass
             f = open('yw', 'r')
         d.display_text_on_line(1, c[0], False, TextAlignment.LEFT, col)
-        d.display_text_on_line(2, "".join(c[1]), False, TextAlignment.LEFT, col)
+        d.display_text_on_line(2, s1, False, TextAlignment.LEFT, col)
         d.display_text_on_line(3, c[2], False, TextAlignment.LEFT, col)
         d.display_text_on_line(4, s2, False, TextAlignment.LEFT, col)
         d.display_text_on_line(6, f.read(), False, TextAlignment.CENTRE, TextColours.CYAN)
