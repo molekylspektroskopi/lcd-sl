b4f0f5c3ea3016d765fbb4c6b8c9c5a5544e93b3
Markus Wiik <vassius@gmail.com>
2013-05-06 08:42:34 +0200

    Remove update interval check and extend sleep instead

--- sl.py
+++ sl.py
@@ -42,33 +42,25 @@ try:
     last_check = 0
     r = re.compile(r'([^ ]+ ([0-9]{2}:[0-9]{2}|[0-9]{1,2} min))') 
     while(True):
-        if time.time() - last_check >= 15:
-            c = api.get_departures(9301)
-            m = re.match(r, c[1])
-            if m:
-                s1 = m.group(0)
-            else:
-                s1 = c[1]
-            m = re.match(r, c[3])
-            if m:
-                s2 = m.group(0)
-            else:
-                s2 = c[3]
-            last_check = time.time()
-            try:
-                f.close()
-            except:
-                pass
-            f = open('yw', 'r')
-            yw = f.read()
-            f.close()
+        c = api.get_departures(9301)
+        m = re.match(r, c[1])
+        if m:
+            s1 = m.group(0)
+        else:
+            s1 = c[1]
+        m = re.match(r, c[3])
+        if m:
+            s2 = m.group(0)
+        else:
+            s2 = c[3]
+        f = open('yw', 'r')
+        yw = f.read()
+        f.close()
         d.display_text_on_line(1, c[0], False, TextAlignment.LEFT, TextColours.ORANGE)
         d.display_text_on_line(2, s1, False, TextAlignment.LEFT, TextColours.ORANGE)
         d.display_text_on_line(3, c[2], False, TextAlignment.LEFT, TextColours.ORANGE)
         d.display_text_on_line(4, s2, False, TextAlignment.LEFT, TextColours.ORANGE)
         d.display_text_on_line(6, yw, False, TextAlignment.CENTRE, TextColours.CYAN)
-#        c[1].rotate(-1)
-#        c[3].rotate(-1)
-        sleep(0.05)
+        sleep(15)
 except KeyboardInterrupt:
     exit()
